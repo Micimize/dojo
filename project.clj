d42176d8357d7a516c5ec469cc696e0505c161d4
micimize <michael.the.awesome@gmail.com>
2014-02-26 17:17:01 -0600

    added shabby datomic code

--- project.clj
+++ project.clj
@@ -2,16 +2,19 @@
   :description "With the goal of Learn by Play."
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.datomic/datomic-free "0.9.4556"]
                  [org.clojure/tools.logging "0.2.6"]
                  [compojure "1.1.5"]
                  [fogus/ring-edn "0.2.0"]
                  [hiccup "1.0.3"]
                  [garden "0.1.0-beta6"]
                  [hiccup-bootstrap "0.1.2"]
+
                  [org.clojure/clojurescript "0.0-2156"]
-                 [hiccups "0.3.0"]
                  [org.clojure/core.async "0.1.256.0-1bf8cf-alpha"]
-                 [lein-cljsbuild "1.0.2"]
+                 [hiccups "0.3.0"]
+                 [me.raynes/fs "1.4.4"]
+                 [lein-cljsbuild "1.0.2" :exclusions [fs]]
                  [jayq "2.5.0"]
                  [cljs-ajax "0.2.3"]
                  [com.andrewmcveigh/cljs-time "0.1.1"]]
--- src/dojo/core.clj
+++ src/dojo/core.clj
@@ -1,5 +1,37 @@
 (ns dojo.core
-  (:require [clojure.tools.logging :as log]))
+  (:require [clojure.tools.logging :as log]
+            [datomic.api :as d]))
 
-(defn save-game [result-set]
-  (prn result-set))
+(defn create-db []
+  (let [uri "datomic:mem://localhost:4334/dojo"]
+    (d/delete-database uri)
+    (d/create-database uri)
+    (d/transact (d/connect uri) (load-file "resources/schema.edn"))))
+
+(create-db)
+(def conn (d/connect "datomic:mem://localhost:4334/dojo"))
+
+(defn save-question [{:keys [a b guess]}]
+  (let [question-id (d/tempid :db.part/user)]
+    (d/transact conn [{:db/id question-id
+                       :question/multiplicand (biginteger a)
+                       :question/multiplier (biginteger b)
+                       :question/guess (biginteger guess)}])
+    question-id))
+
+(defn save-round [round]
+  (let [round-id (d/tempid :db.part/user)]
+    (loop [questions round]
+      (d/transact conn [{:db/id round-id
+                         :round/question (save-question (first questions))}])
+      (when-not (empty? (rest questions))
+        (recur (rest questions))))))
+
+(save-round '({:a 1 :b 2 :guess 3}))
+
+;experimenting with datalog. It's wierd
+(d/q '[:find ?multiplicand ?multiplier ?guess
+       :where [_ :question/guess ?guess]
+              [_ :question/multiplier ?multiplier]
+              [_ :question/multiplicand ?multiplicand]]
+     (d/db conn))
